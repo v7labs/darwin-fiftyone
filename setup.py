3def723477122a133c7653d1097fd1813727f662
Aleksandar Ivic <aleksandar.ivic.coa@gmail.com>
2026-01-20 15:53:57 +0100

    Bump version to 2.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(requirements_path) as f:
 
 setup(
     name="darwin-fiftyone",
-    version="2.1.1",
+    version="2.1.2",
     description="Integration between V7 Darwin and Voxel51",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
